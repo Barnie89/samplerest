47344592804cd59824aebe4fc14aef0c9de4ea06
Barnie89 <n.rajesh3@yahoo.com>
2022-07-25 20:40:25 +0530

    added script

---
+++ JavaCoding/src/codingPractice/firstOccuranceString.java
@@ -0,0 +1,34 @@
+package codingPractice;
+
+public class firstOccuranceString {
+	
+	static char []count= new char[256];
+	static void getcount(String str){
+		for(int i=0;i<str.length();i++){
+			count[str.charAt(i)]++;
+			// gets the count of each char in string
+		}
+	}
+	static int firstoccurance(String str){
+		getcount(str);
+		int index=-1;
+		for(int i=0;i<str.length();i++){
+			if(count[str.charAt(i)]==1){
+				index=i;
+				break;				
+			}
+		}		
+		return index;		
+		
+	}
+
+	public static void main(String[] args) {
+		
+		String str= "geeksforgeeks";
+		int index=firstoccurance(str);
+        System.out.println(index==-1?"string is empty or all char are repeating"
+        	:"first non repeeating char is "+ str.charAt(index)	
+        		);
+	}
+
+}
